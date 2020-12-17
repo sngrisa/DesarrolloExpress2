0005b5eb251158914c784c92df004ff63142d65a
Santiago Grisafi <sngrisa@gmail.com>
2020-12-16 21:20:32 -0300

    Avance con el proyecto

--- app.js
+++ app.js
@@ -8,4 +8,16 @@ app.get("/", function(req,res){
     res.render("index", {nombre : "Pagina de SiG Software", titulo: "Pagina de SiG Sofware"});
 });
 
+app.get("/pratica", function(req,res){
+    res.render("practica");
+});
+
+app.post("/", function(req,res){
+    res.render("envio");
+});
+
+app.get("/index", function(req,res){
+    res.render("index2");
+});
+
 app.listen(8080);
